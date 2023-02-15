e4e9906a89161ce90857670ae33939a532b78b17
HushNet <pakhomov.alx@mail.ru>
2023-02-15 16:51:01 +0300

    Initial commit

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,58 @@
+﻿/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+* 
+* */
+function getReview() {
+    // Создадим объект
+    let comment = {}
+    
+    // Сохраним свойство имени
+    comment["userName"] = prompt("Как вас зовут ?")
+    if(comment["userName"] == null){
+        return
+    }
+    
+    // Сохраним текст отзыва
+    comment["comment"] = prompt("Напишите свой отзыв")
+    if(comment["comment"] == null){
+        return
+    }
+    
+    // Сохраним текущее время
+    comment["date"] = new Date().toLocaleString()
+    
+    let isRated = confirm("Вы хотите, чтобы ваш отзыв имел рейтинг?");
+    
+    if (isRated){
+        let review = Object.create(comment);
+        review["rate"] = 0;
+
+        writeReview(review)
+    }else{
+        writeReview(comment);
+    }
+    
+    // Добавим на страницу
+    
+}
+
+/*
+* Запишем отзыв на страницу 
+* 
+* */
+const writeReview = review => {
+    
+    if (review.hasOwnProperty("rate")){
+        document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+            `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i> ------> Рейтинг: <b>${review['rate']}</b></p>` +
+            `<p>${review['comment']}</p>`  +
+            '</div>';
+    }
+    else{
+        document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+            `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+            `<p>${review['comment']}</p>`  +
+            '</div>';
+    }
+}
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,7 @@
+﻿const saveInput = function (){
+    let input = document.getElementsByClassName("input-field")[0].value;
+    
+    alert("Текущий ввод: " + `${input} ` + "\n Последний ввод: " + `${this.lastInput}`);
+    
+    this.lastInput = input;
+}
